984226fb1acb210d5adc242f818d26d089f85efa
Milanparajuli <milanbhai2058@gmail.com>
2023-05-15 17:48:55 +0545

    initialize commit

---
+++ src/main/java/com/milan/doctorsNepal/DoctorsNepalApplication.java
@@ -0,0 +1,13 @@
+package com.milan.doctorsNepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsNepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsNepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milan/doctors/nepal/doctorsNepal/DoctorsNepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.doctors.nepal.doctorsNepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsNepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
